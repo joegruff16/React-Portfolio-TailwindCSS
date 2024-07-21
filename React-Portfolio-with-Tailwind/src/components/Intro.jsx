ac325b5b1ded28ee03d59844525f470e00440780
Joseph Ruff <joegruff16@gmail.com>
2024-07-21 18:15:59 -0400

    Created this with a tutorial and adjusted the dark mode text color and background to make it look nicer.

--- React-Portfolio-with-Tailwind/src/components/Intro.jsx
+++ React-Portfolio-with-Tailwind/src/components/Intro.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Joseph</h1>
+      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
+        Joseph
+      </h1>
       <p className="text-base md:text-xl mb-3 font-medium">
         Full-stack Developer
       </p>
