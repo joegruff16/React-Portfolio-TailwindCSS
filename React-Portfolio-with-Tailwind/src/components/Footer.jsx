e13b49f240790e9b889479f656df84b4d2b89ee7
Joseph Ruff <joegruff16@gmail.com>
2024-07-21 17:21:10 -0400

    Completed a basic footer without social media icons

--- React-Portfolio-with-Tailwind/src/components/Footer.jsx
+++ React-Portfolio-with-Tailwind/src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="py-5 text-center">
+      {/* Social Icons */}
+      <p className="text-sm mt-2 opacity-50">
+        &copy; {new Date().getFullYear()} Joseph Ruff. All rights reserved.
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
