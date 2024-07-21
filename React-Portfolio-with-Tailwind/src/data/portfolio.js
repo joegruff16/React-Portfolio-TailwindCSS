472567cc85d52517a813b152663023a7d6983fb0
Joseph Ruff <joegruff16@gmail.com>
2024-07-21 15:59:44 -0400

    Completed Portfolio component. Made PortfolioItem compnent and completed that as well. Now I have a basic design for my main page without images.

---
+++ React-Portfolio-with-Tailwind/src/data/portfolio.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: "e-comm-retailer",
+    imgUrl: "",
+    stack: ["Express", "Node", "Javascript", "Postgres", "Sequelize"],
+    link: "https://github.com/joegruff16/e-comm-retailer",
+  },
+  {
+    title: "tutorHub",
+    imgUrl: "",
+    stack: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Bootstrap",
+      "Postgres",
+      "Node",
+      "Express",
+      "Sequelize",
+    ],
+    link: "https://github.com/joegruff16/tutorHub",
+  },
+  {
+    title: "Journeys-End",
+    imgUrl: "",
+    stack: ["HTML", "CSS", "Javascript", "Bootstrap"],
+    link: "https://github.com/joegruff16/Journeys-End",
+  },
+];
